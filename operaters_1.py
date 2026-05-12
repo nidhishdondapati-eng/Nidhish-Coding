276a574d8efe36ffda643de385259df296469750
nidhishdondapati-eng <nidhish.dondapati@gmail.com>
2026-05-12 17:35:54 -0500

    hi

---
+++ operaters_1.py
@@ -0,0 +1,9 @@
+tree1=95
+tree2=92
+tree3=73
+tree4=37
+tree5=38
+sum=tree1+tree2+tree3+tree4+tree5
+print("sum of all trees is",sum)
+average=sum/5
+print("average of all trees is",average)
---
+++ operaters_3.py
@@ -0,0 +1,9 @@
+math=int (input("math"))
+science=int (input("science"))
+tech=int (input("tech"))
+pe=int (input("pe"))
+sum=math+tech+pe+science
+print("sum of all subjects is",sum)
+
+perc=(sum/400)*100
+print("percentage is",perc)
